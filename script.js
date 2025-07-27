10874707645d8ca11e80e6fa0dd9cef8829e7b41
Artem <sales@profgps.ua>
2025-07-27 18:25:09 +0300

    добавил 2 способа написания цикла - закомментировал их в коде

--- script.js
+++ script.js
@@ -21,6 +21,47 @@ for (let i = 0; i < 2; i++) {
     }
 }
 
+// let count = 0;
+
+// while (count < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', '');
+//     const b = prompt('На сколько оцените его?', '');
+
+//     if (
+//         a !== null && b !== null &&
+//         a.trim() !== '' && b.trim() !== '' &&
+//         a.length < 50
+//     ) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//         count++;
+//     } else {
+//         console.log('error');
+//     }
+// }
+
+// function askMovie(count = 0) {
+//     if (count >= 2) return;
+
+//     const a = prompt('Один из последних просмотренных фильмов?', '');
+//     const b = prompt('На сколько оцените его?', '');
+
+//     if (
+//         a !== null && b !== null &&
+//         a.trim() !== '' && b.trim() !== '' &&
+//         a.length < 50
+//     ) {
+//         personalMovieDB.movies[a] = b;
+//         console.log('done');
+//         askMovie(count + 1);
+//     } else {
+//         console.log('error');
+//         askMovie(count);
+//     }
+// }
+
+// askMovie();
+
 
 if  (personalMovieDB.count < 10) {
 console.log("Просмотрено довольно мало фильмов");
